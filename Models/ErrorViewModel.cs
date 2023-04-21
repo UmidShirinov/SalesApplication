aed04cf8262e1945abfc93299f2a079d852e8cf8
UmidShirinov <sirinovumid623@gmail.com>
2023-04-21 11:28:58 +0200

    commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
